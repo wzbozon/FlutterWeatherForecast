2363669122724e8ff72a3c4a9e68925ade9ff7a3
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2024-07-14 21:32:01 +0300

    chore: fix BASE_URL warning

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,13 +6,13 @@ import '/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  static const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
-    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+    final response = await http.get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
