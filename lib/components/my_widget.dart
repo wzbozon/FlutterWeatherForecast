e0cdb48939da7dd6142e0ce8716ff5f74b32aff0
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2024-07-23 04:18:27 +0300

    test: add example widget test

---
+++ lib/components/my_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
